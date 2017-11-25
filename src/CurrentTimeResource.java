caf8b7b0d12af5ec9584b3e38a39b2beb30ba2b2
Roby <roberta.btt@gmail.com>
2017-11-25 11:07:10 +0100

    First test on browser on get a Json simple response

--- src/CurrentTimeResource.java
+++ src/CurrentTimeResource.java
@@ -1,16 +1,35 @@
+import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
-
+import org.json.JSONObject;
 
 public class CurrentTimeResource extends ServerResource {
-
+	
+	
     @Get 
     public Representation getTime() {
+
+   	
+    	//Hot to get Attributes from request:
+    	//String message = "time \"" + request.getAttributes().get("now") + "\"";
+    	     	
         long now = System.currentTimeMillis(); 
         String nowstr = String.valueOf(now); 
-        Representation result = new StringRepresentation(nowstr);
+        
+        
+        JSONObject testJson = new JSONObject();
+        testJson.put("status", "ok");
+        testJson.put("code", "200");
+        testJson.put("message", "");
+        testJson.put("result", nowstr);
+        
+        
+        Representation result = new StringRepresentation(testJson.toString());
+        
+        result.setMediaType(MediaType.APPLICATION_JSON);        
+        
         return result;
     }
 }
--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,7 +1,6 @@
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 import org.restlet.Server;
-import org.restlet.resource.Get;
 
 import org.restlet.resource.ServerResource;
 
@@ -15,19 +14,12 @@ public class ServerMain extends ServerResource {
     	
     	component.getServers().add(server);
     	component.getDefaultHost().attach("/test", ServerMain.class);
-    	component.getDefaultHost().attach("/time", CurrentTimeResource.class);    	
+    	component.getDefaultHost().attach("/time", CurrentTimeResource.class);
+    	component.getDefaultHost().attach("/time/{param}", CurrentTimeResource.class);
     	component.start();
 
 
     }
 
-    @Get
-    public String toStringParam(){
-    	return "Resource URI  : " + getReference() + '\n' + "Root URI      : "
-                + getRootRef() + '\n' + "Routed part   : "
-                + getReference().getBaseRef() + '\n' + "Remaining part: "
-                + getReference().getRemainingPart();
-    }
-    
 
 }
